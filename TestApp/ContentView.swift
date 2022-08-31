8d7f1933818ae2b43b6d078155b8a0e2459ce5e4
hu_f <hu_f@live.com>
2022-08-31 15:57:41 +0300

    Write/Read file on document directory

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -9,31 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var t: Int = 0
+    @State var createText = ""
+    @State var displayText = ""
     
     var body: some View {
         
         VStack{
+            TextEditor(text: $createText)
             HStack{
-                Text("You pressed button ").font(.title)
-                Text("\(t)").font(.title)
+                Button(action: {
+                    let fm = FileManager.default
+                    let urls = fm.urls(for: .documentDirectory, in: .userDomainMask )
+                    let url = urls.last?.appendingPathComponent("file.txt")
+                    do{
+                        try createText.write(to: url!, atomically: true, encoding: String.Encoding.utf8)
+                    }catch{
+                        print("File writing error")
+                    }
+                }){
+                    Text("Write file")
+                }
             }
-            Button(action: {self.t = self.t + 1}){
-                Text("Button").font(.title)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .accentColor(Color.white)
-                    .padding()
+            Spacer()
+            Button(action: {
+                let fm = FileManager.default
+                let urls = fm.urls(for: .documentDirectory, in: .userDomainMask )
+                let url = urls.last?.appendingPathComponent("file.txt")
+                do{
+                    let fileContent = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+                    displayText = fileContent
+                }catch{
+                    print("File reading error")
+                }
+            }){
+                Text("Read file")
             }
-        }
+            TextEditor(text: $displayText)
+        }.padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
             ContentView()
-                .preferredColorScheme(.dark)
-        }
     }
 }
